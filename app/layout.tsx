45ea9034e39524ec62c34b901246f4f702fc96dd
Mark Du <dulongjiebackup@gmail.com>
2024-03-18 11:58:39 -0400

    dynamic metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
-import '@/app/ui/global.css'
+import { Metadata } from "next";
 
+import '@/app/ui/global.css'
 import { inter } from "@/app/ui/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Acme Dashboard",
+    default: "Acme Dashboard"
+  }
+}
 
 export default function RootLayout({
   children,
